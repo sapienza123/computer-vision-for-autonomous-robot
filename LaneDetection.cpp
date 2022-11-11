c4938c1c0b5abfc70d85fa8686d52b1240d46c12
Neeha  Sajja <neeha9chowdary@gmail.com>
2022-11-11 16:38:07 +0100

    Add files via upload

---
+++ LaneDetection.cpp
@@ -0,0 +1,13 @@
+ // This is done using the hough transform method
+ #include<opencv2/imgcodecs.hpp>
+ #include<opencv2/highgui.hpp>
+ #include<opencv2/imgproc.hpp>
+ #include<iostream>
+ #include<algorithm>
+
+ using namespace cv;
+ using namespace std;
+
+ int main() {
+   
+ //edge detection using CaanyEdgeDetection
