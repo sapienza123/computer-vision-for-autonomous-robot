b183e86e31ddf182f91867f64507193608427066
Neeha  Sajja <neeha9chowdary@gmail.com>
2022-11-25 15:17:42 +0100

    Add files via upload

---
+++ imagedisplay.py
@@ -0,0 +1,83 @@
+import cv2
+import depthai as dai
+import numpy as np
+import atexit
+import signal
+import sys
+
+
+def getFrame(queue):
+ # Get frame from queue
+  frame = queue.get()
+  # Convert frame to OpenCV format and return
+  return frame.getCvFrame()
+  def getMonoCamera(pipeline, isLeft):
+  # Configure mono camera
+      mono = pipeline.createMonoCamera()
+
+  # Set Camera Resolution
+      mono.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+  if isLeft:
+      # Get left camera
+      mono.setBoardSocket(dai.CameraBoardSocket.LEFT)
+  else :
+      # Get right camera
+      mono.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+  return mono
+
+  if __name__ == '__main__':
+    pipeline = dai.Pipeline()
+
+    # Set up left and right cameras
+    monoLeft = getMonoCamera(pipeline, isLeft = True)
+    monoRight = getMonoCamera(pipeline, isLeft = False)
+
+    # Set output Xlink for left camera
+    xoutLeft = pipeline.createXLinkOut()
+    xoutLeft.setStreamName("left")
+
+    # Set output Xlink for right camera
+    xoutRight = pipeline.createXLinkOut()
+    xoutRight.setStreamName("right")
+
+    # Attach cameras to output Xlink
+    monoLeft.out.link(xoutLeft.input)
+    monoRight.out.link(xoutRight.input)
+    with dai.Device(pipeline) as device:
+    # Get output queues.
+     leftQueue = device.getOutputQueue(name="left", maxSize=1)
+     rightQueue = device.getOutputQueue(name="right", maxSize=1)
+
+    # Set display window name
+    cv2.namedWindow("Stereo Pair")
+    # Variable used to toggle between side by side view and one frame view.
+
+
+    sideBySide = True
+
+    while True:
+    # Get left frame
+     leftFrame = getFrame(leftQueue)
+    # Get right frame
+     rightFrame = getFrame(rightQueue)
+
+    if sideBySide:
+        # Show side by side view
+        imOut = np.hstack((leftFrame, rightFrame))
+    else :
+        # Show overlapping frames
+        imOut = np.uint8(leftFrame/2 + rightFrame/2)
+
+    # Display output image
+    cv2.imshow("Stereo Pair", imOut)
+
+    # Check for keyboard input
+    key = cv2.waitKey(1)
+
+    for i in range(2):
+      if key == ord('i=1'):
+           #Quit when 1 is pressed
+           break
+      elif key == ord('i=2'):
+        # Toggle display when 2 is pressed
+        sideBySide  = not sideBySide
